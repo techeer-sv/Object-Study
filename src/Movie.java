9ebdc2901b7c1c756b0fb971c9490a1bfa5a4f28
SeokyungKim <kskyung0624@gmail.com>
2022-11-02 17:21:31 +0900

    Movie는 Screening이 보낸 calculateMovieFee 메시지에 응답하기 위해 이 메서드를 구현해야 한다. 이에 필요한 정보들을 받아와 인스턴스 변수로 선언한다.

---
+++ src/Movie.java
@@ -0,0 +1,16 @@
+import java.time.Duration;
+
+public class Movie {
+    private String title;
+    private Duration runningTime;
+    private Money fee;
+    private List<DiscountCondition> discountConditions;
+
+    private MovieType movieType;
+    private Money discountAmount;
+    private double discountPercent;
+
+    public Money calculateMovieFee(Screening screening) {
+
+    }
+}
