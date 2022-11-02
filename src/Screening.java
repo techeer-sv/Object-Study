1954e9311d0b5d9ef2ce8866fa6a4869aef2e965
SeokyungKim <kskyung0624@gmail.com>
2022-11-02 17:15:15 +0900

    Feat : Screening은 Screening의 입장에서만 Movie에 메시지를 보내기 때문에 캡슐화되며 결합도를 낮춘다.

--- src/Screening.java
+++ src/Screening.java
@@ -6,6 +6,11 @@ public class Screening {
     private LocalDateTime whenScreened;
 
     public Reservation reserve(Customer customer, int audienceCount) {
+        return new Reservation(customer, this, calculateFee(audienceCount), audienceCount);
+    }
 
+    private Money calculateFee(int audienceCount) {
+        // 수신자인 Movie가 아니라 송신자인 Screening의 의도를 표현한다 => 캡슐화
+        return movie.calculateMovieFee(this).times(audienceCount);
     }
 }
