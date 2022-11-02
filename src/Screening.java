b8916254f8488d535e51c1ee39d93c565e698154
SeokyungKim <kskyung0624@gmail.com>
2022-11-02 17:09:20 +0900

    Feat : reserve 책임을 수행하기 위해 필요한 인스턴스 변수 설정

---
+++ src/Screening.java
@@ -0,0 +1,11 @@
+import java.time.LocalDateTime;
+
+public class Screening {
+    private Movie movie;
+    private int sequence;
+    private LocalDateTime whenScreened;
+
+    public Reservation reserve(Customer customer, int audienceCount) {
+
+    }
+}
