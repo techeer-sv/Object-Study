c62c181ef88d97545d7f8c921c580e68565a05ba
SeokyungKim <kskyung0624@gmail.com>
2022-11-02 17:45:42 +0900

    Screening은 DiscountCondition의 getWhenScreened, getSequence 메시지를 수행하는 메서드를 구현해야 한다

--- src/Screening.java
+++ src/Screening.java
@@ -9,6 +9,14 @@ public class Screening {
         return new Reservation(customer, this, calculateFee(audienceCount), audienceCount);
     }
 
+    public LocalDateTime getWhenScreened() {
+        return whenScreened;
+    }
+
+    public int getSequence() {
+        return sequence;
+    }
+
     private Money calculateFee(int audienceCount) {
         // 수신자인 Movie가 아니라 송신자인 Screening의 의도를 표현한다 => 캡슐화
         return movie.calculateMovieFee(this).times(audienceCount);
