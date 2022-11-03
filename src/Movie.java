de8342fcd02c91c7d8a36a892dd589ed1cec24ec
SeokyungKim <kskyung0624@gmail.com>
2022-11-03 14:37:39 +0900

    Movie에서 나뉜 DiscountCondition을 각각 바라봐야 하므로 결합도가 높아져 변경과 캡슐화 관점에서는 나빠졌다

--- src/Movie.java
+++ src/Movie.java
@@ -1,10 +1,12 @@
 import java.time.Duration;
+import java.util.List;
 
 public class Movie {
     private String title;
     private Duration runningTime;
     private Money fee;
-    private List<DiscountCondition> discountConditions;
+    private List<PeriodCondition> periodConditions;
+    private List<SequenceCondition> sequenceConditions;
 
     private MovieType movieType;
     private Money discountAmount;
@@ -19,7 +21,16 @@ public class Movie {
     }
 
     private boolean isDiscountable(Screening screening) {
-        return discountConditions.stream()
+        return checkPeriodConditions(screening) || checkSequenceConditions(screening);
+    }
+
+    private boolean checkPeriodConditions(Screening screening) {
+        return periodConditions.stream()
+                .anyMatch(condition -> condition.isSatisfiedBy(screening));
+    }
+
+    private boolean checkSequenceConditions(Screening screening) {
+        return sequenceConditions.stream()
                 .anyMatch(condition -> condition.isSatisfiedBy(screening));
     }
 
