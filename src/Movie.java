4e74a8e188886a4066b9aefc6359eb48ffa037ac
SeokyungKim <kskyung0624@gmail.com>
2022-11-03 15:08:18 +0900

    Movie에서 POLYMORPHISM 패턴과 PROTECTED VARIATIONS 패턴을 활용해 할인 정책을 분리한다. 여기서는 구현을 공유할 필요가 있으므로 인터페이스가 아닌 추상 클래스를 이용한다

--- src/Movie.java
+++ src/Movie.java
@@ -1,19 +1,23 @@
 import java.time.Duration;
+import java.util.Arrays;
 import java.util.List;
 
-public class Movie {
+public abstract class Movie {
     private String title;
     private Duration runningTime;
     private Money fee;
     private List<DiscountCondition> discountConditions;
 
-    private MovieType movieType;
-    private Money discountAmount;
-    private double discountPercent;
+    public Movie(String title, Duration runningTime, Money fee, DiscountCondition... discountConditions) {
+        this.title = title;
+        this.runningTime = runningTime;
+        this.fee = fee;
+        this.discountConditions = Arrays.asList(discountConditions);
+    }
 
     public Money calculateMovieFee(Screening screening) {
         if (isDiscountable(screening)) {
-            return fee.minus(c다alculateDiscountAmount());
+            return fee.minus(calculateDiscountAmount());
         }
 
         return fee;
@@ -23,28 +27,9 @@ public class Movie {
         return discountConditions.stream().anyMatch(condition -> condition.isSatisfiedBy(screening));
     }
 
-    private Money calculateDiscountAmount() {
-        switch (movieType) {
-            case AMOUNT_DISCOUNT:
-                return calculateAmountDiscountAmount();
-            case PERCENT_DISCOUNT:
-                return calculatePercentDiscountAmount();
-            case NONE_DISCOUNT:
-                return calculateNoneDiscountAmount();
-        }
+    abstract protected Money calculateDiscountAmount();
 
-        throw new IllegalStateException();
-    }
-
-    private Money calculateAmountDiscountAmount() {
-        return discountAmount;
-    }
-
-    private Money calculatePercentDiscountAmount() {
-        return fee.times(discountPercent);
-    }
-
-    private Money calculateNoneDiscountAmount() {
-        return Money.ZERO;
+    protected Money getFee() {
+        return fee;
     }
 }
---
+++ src/NoneDiscountMovie.java
@@ -0,0 +1,13 @@
+import java.time.Duration;
+
+public class NoneDiscountMovie extends Movie{
+    public NoneDiscountMovie(String title, Duration runningTime,
+                             Money fee, DiscountCondition... discountConditions) {
+        super(title, runningTime, fee, discountConditions);
+    }
+
+    @Override
+    protected Money calculateDiscountAmount() {
+        return Money.ZERO;
+    }
+}
