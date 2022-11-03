51baf1ad600756cf42cbfa919cb4c4192d095751
SeokyungKim <kskyung0624@gmail.com>
2022-11-03 16:05:55 +0900

    에러 제거를 위한 필요 클래스 구현

--- src/Screening.java
+++ src/Screening.java
@@ -17,6 +17,10 @@ public class Screening {
         return sequence;
     }
 
+    public Movie getMovie() {
+        return movie;
+    }
+
     private Money calculateFee(int audienceCount) {
         // 수신자인 Movie가 아니라 송신자인 Screening의 의도를 표현한다 => 캡슐화
         return movie.calculateMovieFee(this).times(audienceCount);
